dfa540b0c94fbd4c7732e3672e09acc11ff1a3fb
tuhalf <37873266+tuhalf@users.noreply.github.com>
2020-12-25 14:23:45 +0300

    puş

--- SOMvhdl.srcs/sources_1/new/kmap.vhd
+++ SOMvhdl.srcs/sources_1/new/kmap.vhd
@@ -61,7 +61,7 @@ end kmap;
 
 architecture Behavioral of kmap is
     type Specs is array(specCount-1 downto 0) of std_logic_vector (7 DOWNTO 0);
-    type KMapT is array(MapHeight-1 downto 0,MapHeight-1 downto 0) of Specs;
+    type KMapT is array((n_bits(MapHeight-1)*2)-1 downto 0) of Specs;
     signal KMap : KMapT;
     signal outputT: STD_LOGIC_VECTOR((7*specCount)+(specCount-1) downto 0);
     signal readyDRV: STD_LOGIC;
@@ -71,16 +71,21 @@ architecture Behavioral of kmap is
     signal bestOfCores: bestOfCoresT;
     constant cycleCount: natural := ((MapHeight*MapHeight)/coresCount);
     signal coresDone: std_logic_vector(coresCount-1 downto 0);
-    constant k: integer := 0;
+    constant j: integer := 0;
 begin
 --------output-------
 outputP: process(clk, rst)
+variable x,y:std_logic_vector(n_bits(MapHeight-1)-1 downto 0);
+variable comb: std_logic_vector((n_bits(MapHeight-1)*2)-1 downto 0);
 begin
     if rst = '1' then
         outputT <= (others => '0');
     elsif rising_edge(clk) then
         for j in 0 to specCount-1 loop
-            outputT((7+(j*8)) downto (0+(j*8))) <= KMap(XPos,YPos)(j);
+            x:= std_logic_vector(to_unsigned(XPos,n_bits(MapHeight-1)));
+            y:= std_logic_vector(to_unsigned(YPos,n_bits(MapHeight-1)));
+            comb:= x&y;
+            outputT((7+(j*8)) downto (0+(j*8))) <= KMap(to_integer(unsigned(comb)))(j);
         end loop;
     end if;
 end process outputP;
@@ -89,10 +94,12 @@ ValueCur <= outputT;
 
 ----------Init--------------
 InitP: process(clk, rst)
+variable x,y:std_logic_vector(n_bits(MapHeight-1)-1 downto 0);
+variable comb: std_logic_vector((n_bits(MapHeight-1)*2)-1 downto 0);
 begin
     if rst = '1' then
         readyDRV<='0';
-        kmap<= (others=>(others=>(others=>(others=>'0'))));
+        --kmap<= (others=>(others=>(others=>(others=>'0'))));
         xCP<= 0;
         yCP<= 0;
     elsif rising_edge(clk) then
@@ -100,14 +107,20 @@ begin
             if not(yCP = MapHeight-1) then
                 --set   
                 for k in 0 to specCount-1 loop
-                    KMap(xCP,yCP) (k) <= RandByte((7+(k*8)) downto (0+(k*8)));
+                    x:= std_logic_vector(to_unsigned(xCP,n_bits(MapHeight-1)));
+                    y:= std_logic_vector(to_unsigned(yCP,n_bits(MapHeight-1)));
+                    comb:= x&y;
+                    KMap(to_integer(unsigned(comb))) (k) <= RandByte((7+(k*8)) downto (0+(k*8)));
                 end loop;
                 --inc
                 yCP <= yCP + 1;
             elsif ycp = MapHeight-1 and not(xCP = MapHeight-1) then
                 --set
                 for k in 0 to specCount-1 loop
-                    KMap(xCP,yCP) (k) <= RandByte((7+(k*8)) downto (0+(k*8)));
+                    x:= std_logic_vector(to_unsigned(xCP,n_bits(MapHeight-1)));
+                    y:= std_logic_vector(to_unsigned(yCP,n_bits(MapHeight-1)));
+                    comb:= x&y;
+                    KMap(to_integer(unsigned(comb))) (k) <= RandByte((7+(k*8)) downto (0+(k*8)));
                 end loop;
                 --inc
                 xCP <= xCP+1;
@@ -115,7 +128,10 @@ begin
             elsif ycp = MapHeight-1 and xCP = MapHeight-1 then
                 --sth
                 for k in 0 to specCount-1 loop
-                    KMap(xCP,yCP) (k) <= RandByte((7+(k*8)) downto (0+(k*8)));
+                    x:= std_logic_vector(to_unsigned(xCP,n_bits(MapHeight-1)));
+                    y:= std_logic_vector(to_unsigned(yCP,n_bits(MapHeight-1)));
+                    comb:= x&y;
+                    KMap(to_integer(unsigned(comb))) (k) <= RandByte((7+(k*8)) downto (0+(k*8)));
                 end loop;
                 readyDRV<='1';
             end if;
@@ -127,12 +143,17 @@ ready<=readyDRV;
 ---------Train---------
 trainP: process(clk)
     variable kmapTemp: std_logic_vector(17 downto 0);
+    variable x,y:std_logic_vector(n_bits(MapHeight-1)-1 downto 0);
+    variable comb: std_logic_vector((n_bits(MapHeight-1)*2)-1 downto 0);
 begin
     if rising_edge(clk) then
         if train = '1' then
             for k in 0 to specCount-1 loop
-                kmapTemp:= std_logic_vector(unsigned(KMap(xCP,yCP) (k)) + ((LNRate*(unsigned(input((7+(k*8)) downto (0+(k*8)))) - unsigned(KMap(xCP,yCP) (k))))/to_unsigned(rateSensetivity,n_bits(rateSensetivity))));
-                KMap(xCP,yCP) (k) <= kmapTemp(17 downto 10);
+                x:= std_logic_vector(to_unsigned(xCP,n_bits(MapHeight-1)));
+                y:= std_logic_vector(to_unsigned(yCP,n_bits(MapHeight-1)));
+                comb:= x&y;
+                kmapTemp:= std_logic_vector(unsigned(KMap(to_integer(unsigned(comb))) (k)) + ((LNRate*(unsigned(input((7+(k*8)) downto (0+(k*8)))) - unsigned(KMap(to_integer(unsigned(comb))) (k))))/to_unsigned(rateSensetivity,n_bits(rateSensetivity))));
+                KMap(to_integer(unsigned(comb))) (k) <= kmapTemp(17 downto 10);
             end loop;
         end if;
     end if;
@@ -172,28 +193,33 @@ end process trainP;
 --    end if;
 --end process bmuF;
 
---cores: for k in 0 to coresCount-1 generate
+--cores: for j in 0 to coresCount-1 generate
     core: process(clk, rst)
+    variable x,y:std_logic_vector(n_bits(MapHeight-1)-1 downto 0);
+    variable comb: std_logic_vector((n_bits(MapHeight-1)*2)-1 downto 0);
     variable distance: natural;
     begin
         if rst = '1' then
-            bestOfCores(k)<= (others => 0);
-            coresDone(k)<='0';
+            bestOfCores(j)<= (others => 0);
+            coresDone(j)<='0';
         elsif rising_edge(clk) then
             if FindBMU ='1' then
-                if bestOfCores(k)(3)<cycleCount then
+                if bestOfCores(j)(3)<cycleCount then
                     distance:= 0;
                     for cs in 0 to specCount-1 loop
-                        distance := distance + abs(to_integer(unsigned(input((7+(cs*8)) downto (0+(cs*8))))) - to_integer(unsigned(KMap((bestOfCores(k)(3)/MapHeight),(bestOfCores(k)(3) rem MapHeight)) (cs))));
+                        x:= std_logic_vector(to_unsigned((bestOfCores(j)(3)/MapHeight),n_bits(MapHeight-1)));
+                        y:= std_logic_vector(to_unsigned((bestOfCores(j)(3) rem MapHeight),n_bits(MapHeight-1)));
+                        comb:= x&y;
+                        distance := distance + abs(to_integer(unsigned(input((7+(cs*8)) downto (0+(cs*8))))) - to_integer(unsigned(KMap(to_integer(unsigned(comb))) (cs))));
                     end loop;
-                    if distance<bestOfCores(k)(2) then
-                        bestOfCores(k)(2) <= distance;
-                        bestOfCores(k)(1) <= (bestOfCores(k)(3)/MapHeight);
-                        bestOfCores(k)(0) <= (bestOfCores(k)(3) rem MapHeight);
+                    if distance<bestOfCores(j)(2) then
+                        bestOfCores(j)(2) <= distance;
+                        bestOfCores(j)(1) <= (bestOfCores(j)(3)/MapHeight);
+                        bestOfCores(j)(0) <= (bestOfCores(j)(3) rem MapHeight);
                     end if;
-                    bestOfCores(k)(3) <= bestOfCores(k)(3) +1;
+                    bestOfCores(j)(3) <= bestOfCores(j)(3) +1;
                 else
-                    coresDone(k)<='1';
+                    coresDone(j)<='1';
                 end if;
             end if;
         end if;
